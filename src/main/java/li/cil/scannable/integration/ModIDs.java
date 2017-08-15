69c629a04fe4f2eddb981f09f47ed11075e6cbdd
Florian Nücke <florian@nuecke.de>
2017-08-15 21:13:02 +0200

    1.10 adjustments.

--- src/main/java/li/cil/scannable/integration/ModIDs.java
+++ src/main/java/li/cil/scannable/integration/ModIDs.java
@@ -1,7 +1,7 @@
 package li.cil.scannable.integration;
 
 public class ModIDs {
-    public static final String IC2 = "ic2";
+    public static final String IC2 = "IC2";
     public static final String RedstoneFlux = "CoFHAPI|energy";
 
     private ModIDs() {
