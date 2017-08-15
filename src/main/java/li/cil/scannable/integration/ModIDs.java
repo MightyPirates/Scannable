7ea2499308c8bd44ff5e0dcfa831d9c8cd725b48
Florian Nücke <florian@nuecke.de>
2017-08-15 20:58:24 +0200

    Add classic RedstoneFlux power support.

--- src/main/java/li/cil/scannable/integration/ModIDs.java
+++ src/main/java/li/cil/scannable/integration/ModIDs.java
@@ -2,6 +2,7 @@ package li.cil.scannable.integration;
 
 public class ModIDs {
     public static final String IC2 = "ic2";
+    public static final String RedstoneFlux = "CoFHAPI|energy";
 
     private ModIDs() {
     }
