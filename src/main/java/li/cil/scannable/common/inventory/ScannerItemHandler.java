2493986132e7a1392daea1504cc7fec95331c84e
Florian Nücke <florian@nuecke.de>
2022-04-18 16:10:16 +0200

    Fix modules not staying in the slots they were put into.

--- src/main/java/li/cil/scannable/common/inventory/ScannerItemHandler.java
+++ src/main/java/li/cil/scannable/common/inventory/ScannerItemHandler.java
@@ -3,6 +3,7 @@ package li.cil.scannable.common.inventory;
 import li.cil.scannable.common.item.ScannerItem;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
@@ -12,6 +13,7 @@ public final class ScannerItemHandler extends SimpleContainer {
     public static final int TOTAL_MODULE_COUNT = ACTIVE_MODULE_COUNT;
 
     private static final String TAG_ITEMS = "items";
+    private static final String TAG_SLOT = "slot";
 
     private final ItemStack container;
 
@@ -54,5 +56,39 @@ public final class ScannerItemHandler extends SimpleContainer {
 
     public NonNullList<ItemStack> getActiveModules() {
         return getItemsInRange(0, ACTIVE_MODULE_COUNT);
+    }
+
+    // --------------------------------------------------------------------- //
+    // SimpleContainer
+
+    public void fromTag(final ListTag tag) {
+        for (int i = 0; i < this.getContainerSize(); ++i) {
+            this.setItem(i, ItemStack.EMPTY);
+        }
+
+        for (int i = 0; i < tag.size(); ++i) {
+            final CompoundTag slotTag = tag.getCompound(i);
+            final int slot = slotTag.getByte(TAG_SLOT);
+            if (slot >= 0 && slot < this.getContainerSize()) {
+                slotTag.remove(TAG_SLOT);
+                this.setItem(slot, ItemStack.of(slotTag));
+            }
+        }
+    }
+
+    public ListTag createTag() {
+        final ListTag tag = new ListTag();
+
+        for (int i = 0; i < this.getContainerSize(); ++i) {
+            final ItemStack stack = this.getItem(i);
+            if (!stack.isEmpty()) {
+                final CompoundTag slotTag = new CompoundTag();
+                stack.save(slotTag);
+                slotTag.putByte(TAG_SLOT, (byte) i);
+                tag.add(slotTag);
+            }
+        }
+
+        return tag;
     }
 }
