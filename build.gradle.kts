84cfd8e45bc81d8cd87b2ea0ec69ef9f388a1c6e
Florian Nücke <florian@nuecke.de>
2026-08-24 21:40:18 +0200

    Fix URL oopsie and general cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -194,4 +194,4 @@ tasks.named("build") {
 registerGameTestTask()
 registerLintTask()
 registerApiJarTask(minecraftVersion)
-configureMavenPublishing(minecraftVersion, "https://github.com/fnuecke/Scannable")
+configureMavenPublishing(minecraftVersion, "https://github.com/MightyPirates/Scannable")
